d0bd78db8f72f59cb6368d47e16febdeb551fa8e
GilesBradshaw <giles.bradshaw@sigyl.com>
2012-03-16 00:44:24 +0000

    Tests updated to test substituting an alternative binding provider

--- src/templating/templateEngine.js
+++ src/templating/templateEngine.js
@@ -8,7 +8,7 @@
 //            // - bindingContext.$data is the data you should pass into the template
 //            //   - you might also want to make bindingContext.$parent, bindingContext.$parents, 
 //            //     and bindingContext.$root available in the template too
-//            // - options gives you access to any other properties set on "data-bind: { template: options }"
+//            // - options gives you access to any other properties set on "data-bind: { template: options }  (where data-bind is the configured binding attribute)"
 //            //
 //            // Return value: an array of DOM nodes
 //        }
@@ -20,7 +20,7 @@
 //            //               For example, the jquery.tmpl template engine converts 'someScript' to '${ someScript }' 
 //        }
 //
-//     This is only necessary if you want to allow data-bind attributes to reference arbitrary template variables.
+//     This is only necessary if you want to allow binding attributes to reference arbitrary template variables.
 //     If you don't want to allow that, you can set the property 'allowTemplateRewriting' to false (like ko.nativeTemplateEngine does)
 //     and then you don't need to override 'createJavaScriptEvaluatorBlock'.
 
