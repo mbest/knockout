ab6d37fe43f4443424a6f75457a80feb11eede5f
Michael Best <mbest@dasya.com>
2012-07-20 17:44:58 -1000

    Add spec for 'let' binding

--- src/binding/defaultBindings.js
+++ src/binding/defaultBindings.js
@@ -446,7 +446,7 @@ ko.bindingHandlers['withlight'] = {
 ko.bindingHandlers['let'] = {
     'flags': bindingFlags_contentBind | bindingFlags_canUseVirtual,
     'init': function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-        // Make a modified binding context, with a extra properties, and apply it to descendant elements
+        // Make a modified binding context, with extra properties, and apply it to descendant elements
         var innerContext = bindingContext['extend'](valueAccessor);
         ko.applyBindingsToDescendants(innerContext, element);
     }
